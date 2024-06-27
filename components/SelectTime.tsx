a3ef044b0165255fcc304eab2e300a55b77ae8f8
SerhiiSamara <ndaair@gmail.com>
2024-06-27 19:28:40 +0300

    Create mobile CreateClientForm

--- components/SelectTime.tsx
+++ components/SelectTime.tsx
@@ -86,19 +86,39 @@ const SelectTime = ({ selectOptions,
 				<div className='absolute top-1/2 -translate-y-1/2  md:left-7 left-4 font-montserrat font-normal text-lg leading-6'>
 					{isOpen && !selectedOption ? startValue : selectedOption}</div>
 				{isOpen ?
-					<Image
-						src="/svg/arrow-up.svg"
-						alt="Arrov up icon"
-						width={32}
-						height={32}
-						className="absolute top-1/2 -translate-y-1/2 md:right-7 right-2 ml-auto cursor-pointer"
-					/> : <Image
-						src="/svg/arrow-down.svg"
-						alt="Arrov down icon"
-						width={32}
-						height={32}
-						className="absolute top-1/2 -translate-y-1/2 md:right-7 right-2 ml-auto cursor-pointer"
-					/>}
+					<>
+						<Image
+							src="/svg/arrow-up.svg"
+							alt="Arrov up icon"
+							width={32}
+							height={32}
+							className="hidden md:block absolute top-1/2 -translate-y-1/2 md:right-7 right-2 ml-auto cursor-pointer"
+						/>
+						<Image
+							src="/svg/arrow-up.svg"
+							alt="Arrov up icon"
+							width={24}
+							height={24}
+							className="md:hidden absolute top-1/2 -translate-y-1/2 md:right-7 right-2 ml-auto cursor-pointer"
+						/>
+					</>
+					:
+					<>
+						<Image
+							src="/svg/arrow-down.svg"
+							alt="Arrov down icon"
+							width={32}
+							height={32}
+							className="hidden md:block absolute top-1/2 -translate-y-1/2 md:right-7 right-2 ml-auto cursor-pointer"
+						/>
+						<Image
+							src="/svg/arrow-down.svg"
+							alt="Arrov down icon"
+							width={24}
+							height={24}
+							className="md:hidden absolute top-1/2 -translate-y-1/2 md:right-7 right-2 ml-auto cursor-pointer"
+						/>
+					</>}
 			</div>
 
 			{isOpen && <div className={`${widthValue ? ` w-[${widthValue}px]` : `w-full`} absolute overflow-auto h-60 -ml-[1px] -mt-[2px] bg-white rounded-b-[18px] border-[1px] border-[#E6E6E6] border-t-0`} ref={selectBodyRef}>
