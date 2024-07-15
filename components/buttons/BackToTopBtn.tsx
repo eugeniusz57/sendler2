e88f46237ea50d57ea50938ae075f95d81dbe839
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2024-07-15 17:39:17 +0200

    Update BackToTopBtn.tsx

--- components/buttons/BackToTopBtn.tsx
+++ components/buttons/BackToTopBtn.tsx
@@ -33,14 +33,14 @@ const BackToTopBtn = () => {
 
   return (
     <button
-    className={`fixed flex items-center bottom-5  right-5 bg-transparent border-2 border-greenBtn text-greenBtn hover:text-white rounded-full hover:bg-greenBtn hover:scale-105 hover:border-transparent transition-all text-center w-12 h-12 pl-[8px] text-3xl ${
+    className={`fixed flex items-center right-auto bottom-5   bg-transparent border-2 border-greenBtn text-greenBtn  rounded-full  md:hover:scale-105  transition-all text-center w-12 h-12 pl-[8px] text-3xl ${
       isVisible ? "opacity-100" : "opacity-0"
     } ${
-      "sm:w-16 sm:h-16 sm:pl-[12px] sm:text-4xl"
+      "sm:w-16 sm:h-16 sm:pl-[20px] sm:text-4xl"
     } ${
-      "md:w-20 md:h-20 md:pl-[14px] md:text-5xl"
+      "md:w-20 md:h-20 md:pl-[24px] md:text-5xl"
     } ${
-      "lg:w-24 lg:h-24 lg:pl-[18px] lg:text-6xl"
+      "lg:w-24 lg:h-24 lg:pl-[20px] lg:text-6xl"
     }`}
       onClick={scrollToTop}
     >
