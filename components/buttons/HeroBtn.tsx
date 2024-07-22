1b9f2211c017809113e7b1cfbf172311b8815c5f
SerhiiSamara <ndaair@gmail.com>
2024-07-23 01:48:22 +0300

    mobile and tablet for main page

--- components/buttons/HeroBtn.tsx
+++ components/buttons/HeroBtn.tsx
@@ -4,20 +4,20 @@ import Link from "next/link";
 import { ReactElement } from "react";
 
 function HeroBtn({
-  children,
-  linkTo,
+	children,
+	linkTo,
 }: {
-  children: React.ReactNode;
-  linkTo: string;
+	children: React.ReactNode;
+	linkTo: string;
 }) {
-  return (
-    <Link
-      href={linkTo}
-      className=" w-[144px] py-[10px] rounded-[14px] bg-[#32BB79] hover:bg-green-700 text-white text-base flex items-center justify-center transition-colors"
-    >
-      {children}
-    </Link>
-  );
+	return (
+		<Link
+			href={linkTo}
+			className="w-[156px] md:w-[144px] py-[10px] rounded-[14px] bg-[#32BB79] hover:bg-green-700 text-white text-base flex items-center justify-center transition-colors"
+		>
+			{children}
+		</Link>
+	);
 }
 
 export default HeroBtn;
