e9b40ee919770ffa16a3f3772b5818bfb0cbba8a
SerhiiSamara <ndaair@gmail.com>
2025-01-21 19:52:44 +0200

    Fix befor meeting

--- api-actions/fetchUserArrayHistoryIdInProgress.ts
+++ api-actions/fetchUserArrayHistoryIdInProgress.ts
@@ -4,7 +4,8 @@ import { QueryResult } from "pg";
 import { IResAlfaNames } from "@/globaltypes/types";
 
 export default async function fetchUserArrayHistoryIdInProgress(id: number): Promise<QueryResult<IResAlfaNames>> {
-	const res: QueryResult<IResAlfaNames> = await db.query(`SELECT DISTINCT rs.history_id
+	const res: QueryResult<IResAlfaNames> = await db.query(
+		`SELECT DISTINCT rs.history_id
 FROM recipients_status rs
 INNER JOIN sending_history sh ON sh.history_id = rs.history_id
 WHERE rs.recipient_status = 'pending' AND sg.user_id = ${id}`
