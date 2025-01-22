c262fae0f1c2884609a93244ab873f43dd35285f
Serhii <104717475+SerhiiSamara@users.noreply.github.com>
2025-01-22 07:22:30 +0200

    Account 1 (#106)
    * Fix time zone
    
    * Fix befor meeting
    
    * Change timestapz on timestamp

--- app/api/reseller/helpers/addSendingHistory.ts
+++ app/api/reseller/helpers/addSendingHistory.ts
@@ -6,14 +6,14 @@ import { ISendHistoryDatabase } from "@/globaltypes/types";
 export const addSendingHistory = async (idArray: number[], text: string, method: 'api' | 'web', userName: string, second?: number): Promise<ISendHistoryDatabase> => {
 
 	let res: QueryResult<ISendHistoryDatabase>;
+	// let time: QueryResult<Date>;
 	if (second) {
-		await db.query(`SET TIME ZONE 'Europe/Vilnius'`)
+		await db.query(`SET TIME ZONE 'Europe/Vilnius'`);
+		// time = await db.query(`SELECT to_char(now() AT TIME ZONE 'Europe/Rome','DD.MM.YYYY HH24: MI: SS')`)
+		// console.log('TIME', time.rows[0])
 		res = await db.query(
-			`INSERT INTO sending_history (send_method, text_sms, sending_group_date, alfa_name) VALUES ('${method}', '${text}', now()::timestamptz(0) AT TIME ZONE 'Europe/Vilnius' + interval '${second} second','${userName}') RETURNING *`
+			`INSERT INTO sending_history (send_method, text_sms, sending_group_date, alfa_name) VALUES ('${method}', '${text}', now()::timestamp(0) AT TIME ZONE 'Europe/Vilnius' + interval '${second} second','${userName}') RETURNING *`
 		);
-		// res = await db.query(
-		// 	`INSERT INTO sending_history (send_method, text_sms, sending_group_date, alfa_name) VALUES ('${method}', '${text}', now()::timestamptz(0) + interval '${second} second','${userName}') RETURNING *`
-		// );
 	} else {
 		res = await db.query(
 			`INSERT INTO sending_history (send_method, text_sms, alfa_name) VALUES ('${method}', '${text}', '${userName}') RETURNING *`,
